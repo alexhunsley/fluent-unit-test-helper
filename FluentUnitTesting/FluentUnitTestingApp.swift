1ddc79a38465b183bd55ca670bd2ff6456255f15
Alex Hunsley <alex.hunsley@gmail.com>
2025-07-15 23:03:40 +0100

    Initial Commit

---
+++ FluentUnitTesting/FluentUnitTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  FluentUnitTestingApp.swift
+//  FluentUnitTesting
+//
+//  Created by Alex Hunsley on 15/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct FluentUnitTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FluentUnitTestingTests/FluentUnitTestingTests.swift
@@ -0,0 +1,17 @@
+//
+//  FluentUnitTestingTests.swift
+//  FluentUnitTestingTests
+//
+//  Created by Alex Hunsley on 15/07/2025.
+//
+
+import Testing
+@testable import FluentUnitTesting
+
+struct FluentUnitTestingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
